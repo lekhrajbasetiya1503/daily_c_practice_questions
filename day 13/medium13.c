0b1fb79e5ab2300dac2e95c678dd1b60faa76c99
lekhraj basetiya <142377945+lekhrajbasetiya1503@users.noreply.github.com>
2025-06-26 23:16:48 +0530

    Add files via upload

---
+++ day 13/medium13.c
@@ -0,0 +1,41 @@
+/*
+problem name : 
+
+Write a C program to find highest comman factore.
+
+*/
+
+#include<stdio.h>
+
+int min(int a,int b){
+    if(a<b) return a;
+    else return b;
+}
+
+int hcf(int a,int b)
+{
+    int max;
+    for(int i=min(a,b);i>=1;i--)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            max = i;
+            break;
+        }
+    }
+    return max;
+
+}
+
+int main()
+{
+    int a,b;
+    printf("enter the value of a : ");
+    scanf("%d",&a);
+    printf("enter the value of b : ");
+    scanf("%d",&b);
+
+    printf("the hcf of %d and %d is : %d\n",a,b,hcf(a,b));
+
+
+}
